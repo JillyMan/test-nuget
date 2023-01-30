d0a2ef370712531954a874f690fb51b9aa335d7d
AzureAD\ArtsiomDubinevich <ArtsiomDubinevich@aras.com>
2023-01-30 13:36:20 +0100

    сщььше

---
+++ Test.Core/Test.Core/Consts.cs
@@ -0,0 +1,10 @@
+﻿namespace Test.Core;
+
+public class Consts
+{
+	public static string[] Summaries = new[]
+	{
+		"Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+	};
+
+}
